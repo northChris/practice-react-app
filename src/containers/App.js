d75ca68f5a6551c370a3be3c3e363dcbd3627c22
Admin <admin@Admins-MacBook-Pro.local>
2018-09-19 20:01:18 -0400

    cleaned up code with the use of componets

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -43,48 +44,23 @@ togglePersonsHandler = () => {
 }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
     if(this.state.showPersons){
-      persons = (
-        <div>
-        {this.state.persons.map((person, index) =>{
-          return <Person
-            click={() => this.deletePersonHandler(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)}/>
-        })}
-        </div>
-      );
-      style.backgroundColor = 'red';
-
+      persons = <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />;
     }
 
-    const classes = [];
-    if (this.state.persons.length <= 2){
-      classes.push('red');
-    }
-    if (this.state.persons.length <= 1){
-      classes.push('bold');
-    }
+
 
     return (
-      <div className="App">
-        <h1>hi im a react developer</h1>
-        <p className={classes.join(' ')}>This is really working</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>toggle cards</button>
+      <div className={classes.App}>
+        <Cockpit
+        showPersons={this.state.showPersons}
+        persons={this.state.persons}
+        clicked={this.togglePersonsHandler}/>
         {persons}
       </div>
     );
